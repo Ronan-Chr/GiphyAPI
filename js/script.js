5698232ac86c470de2dd3b9f3c6868a4ba4eee06
Ronan-Chr <ronan2249@gmail.com>
2026-03-29 00:21:32 -0400

    Added giphy api linking and JS logic to make sure it was retrieving informatio from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("script.js loaded");
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=NUJPrI00Q1qmCiRl2HUqnEGZYnE08xZI&q=kaleidoscope&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+let images = [];
+
+fetch(endpoint)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+
+    // loop through gifs
+    for (let i = 0; i < data.data.length; i++) {
+      images.push(data.data[i].images.original.url);
+    }
+
+    // proof it worked (on page, not console)
+    document.querySelector("#gif-container").innerHTML = images.length + " gifs loaded";
+
+  });
